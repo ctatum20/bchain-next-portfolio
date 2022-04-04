b165218ea7aa1ae2a1a8598701a1deb878d441b8
ctatum20 <mocca@adkaddy.com>
2022-04-04 03:32:17 -0500

    UPDATE: personalized portfolio

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,7 +2,7 @@ const AboutPage = () => {
   return (
     <>
       <h1>About Page</h1>
-      <h4>My name is Charnice Tatum. I am a Blockchain Engineer looking to help other companies build user friendly applications and smart contracts.</h4>
+      <h4>My name is Charnice Tatum. I am a Blockchain Developer looking to help other companies build user friendly web3 applications and smart contracts.</h4>
     </>
   );
 };
